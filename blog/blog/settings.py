5f20503bf7ff3dfb82f19debb5c81ee769232259
Maxcodeman <107112162+Maxcodeman@users.noreply.github.com>
2023-11-26 10:56:43 +0800

    Redis配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -125,3 +125,24 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Redis数据库的配置
+CACHES={
+    "default":{#默认
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1:6379/0",
+        "OPTIONS":{
+        "CLIENT_CLASS":"django_redis.client.DefaultClient",
+    }
+},
+    "session":{#session
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":"redis://127.0.0.1;6379/1",
+        "OPTIONS":{
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+}
+#session由数据库存储改为redis存储
+SESSION_ENGINE="django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS="session"
