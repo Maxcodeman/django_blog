e7713be9b773364e7a1dea54696608b13a3ffaad
Maxcodeman <107112162+Maxcodeman@users.noreply.github.com>
2023-12-03 23:07:55 +0800

    博客详情

--- blog/home/admin.py
+++ blog/home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from home.models import ArticleCategory, Article
+from home.models import ArticleCategory, Article, Comment
 
 #注册模型
 admin.site.register(ArticleCategory)
 
 admin.site.register(Article)
+
+admin.site.register(Comment)
--- blog/home/views.py
+++ blog/home/views.py
@@ -1,9 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
-from home.models import ArticleCategory, Article
+from home.models import ArticleCategory, Article, Comment
 
 
 # Create your views here.
@@ -50,3 +50,93 @@ class IndexView(View):
 
         return render(request,"index.html",context=context)
 
+class DetailView(View):
+    def get(self,request):
+        #获取文档id
+        id=request.GET.get('id')
+
+        #获取博客分类信息
+        categories=ArticleCategory.objects.all()
+
+        try:
+            article=Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return render(request,'404.html')
+        else:
+            article.total_views+=1
+            article.save()
+
+        #获取热点数据
+        hot_articles=Article.objects.order_by('-total_views')[:9]
+
+        # 获取分页参数
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+
+        # 获取当前文章的评论数据
+        comments = Comment.objects.filter(article=article).order_by('-created')
+        #获取评论总数
+        total_count=comments.count()
+
+        # 创建分页器
+        paginator = Paginator(comments, page_size)
+
+        # 进行分页处理
+        try:
+            page_comments = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('empty page')
+
+        # 总页数
+        total_pages = paginator.num_pages
+
+        context={
+            "categories":categories,
+            "category":article.category,
+            "article":article,
+            "comments":page_comments,
+            'hot_articles':hot_articles,
+            'page_num':page_num,
+            'page_size':page_size,
+            'total_count':total_count,
+            'total_pages':total_pages,
+        }
+
+        return render(request,"detail.html",context=context)
+
+    def post(self,request):
+        #获取用户信息
+        user=request.user
+
+        #判断用户是否登录
+        if user and user.is_authenticated:
+            #接收参数
+            id=request.POST.get("id")
+            content=request.POST.get("content")
+
+            #判断文章是否存在
+            try:
+                article=Article.objects.get(id=id)
+            except Article.DoesNotExist:
+                return HttpResponseNotFound('没有此文章')
+
+            #保存到数据
+            Comment.objects.create(
+                content=content,
+                article=article,
+                user=user
+            )
+
+            #修改文章评论数量
+            article.comments_count+=1
+            article.save()
+
+            #拼接跳转路由
+            from django.urls import reverse
+            path=reverse('home:detail')+'?id={}'.format(article.id)
+            return redirect(path)
+
+        else:
+            #没有登录则跳转到登录页面
+            from django.urls import reverse
+            return redirect(reverse('users:login'))
